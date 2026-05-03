9124c5b00ec209b7db241fd1c47e229bbfaccb57
CloCliClo0 <hugo22042006@gmail.com>
2026-05-03 19:35:00 +0200

    Finalize API replacement and root project cleanup

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -22,4 +22,11 @@ function requireAuth(req, res, next) {
   }
 }
 
-module.exports = { requireAuth };
+function requireAdmin(req, res, next) {
+  if (!req.user || !req.user.is_admin) {
+    return res.status(403).json({ message: 'Admin access required' });
+  }
+  return next();
+}
+
+module.exports = { requireAuth, requireAdmin };
